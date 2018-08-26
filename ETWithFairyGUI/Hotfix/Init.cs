682155358dbfd0f61c9e1f522adb9ce1155279f3
Kimi2016 <Heroan2010@126.com>
2018-08-26 15:22:18 +0800

    ETWithFairyGUI

---
+++ ETWithFairyGUI/Hotfix/Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ETHotfix
+{
+	public static class Init
+	{
+		public static void Start()
+		{
+			try
+			{
+				
+			}
+			catch (Exception e)
+			{
+				
+			}
+		}
+
+		public static void Update()
+		{
+			try
+			{
+				
+			}
+			catch (Exception e)
+			{
+				
+			}
+		}
+
+		public static void LateUpdate()
+		{
+			try
+			{
+				
+			}
+			catch (Exception e)
+			{
+				
+			}
+		}
+
+		public static void OnApplicationQuit()
+		{
+			
+		}
+	}
+}
