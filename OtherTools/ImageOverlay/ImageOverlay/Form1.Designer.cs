2da80db7f6d2ff5661a4f70bb39e3ab0183cc20e
Kimi2016 <Heroan2010@126.com>
2018-12-17 15:48:59 +0800

    ImageOverlay

---
+++ OtherTools/ImageOverlay/ImageOverlay/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageOverlay
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button_addList = new System.Windows.Forms.Button();
+			this.listBox_allFileName = new System.Windows.Forms.ListBox();
+			this.button_clearList = new System.Windows.Forms.Button();
+			this.pictureBox_imageOverlay = new System.Windows.Forms.PictureBox();
+			this.button_saveImageOverlay = new System.Windows.Forms.Button();
+			this.button_addImage = new System.Windows.Forms.Button();
+			this.button_removeImage = new System.Windows.Forms.Button();
+			this.pictureBox_preview = new System.Windows.Forms.PictureBox();
+			this.label_offsetX = new System.Windows.Forms.Label();
+			this.textBox_inputOffsetX = new System.Windows.Forms.TextBox();
+			this.label_OffsetY = new System.Windows.Forms.Label();
+			this.textBox_inputOffsetY = new System.Windows.Forms.TextBox();
+			this.button_clearImageOverlay = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_imageOverlay)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_preview)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button_addList
+			// 
+			this.button_addList.Location = new System.Drawing.Point(202, 12);
+			this.button_addList.Name = "button_addList";
+			this.button_addList.Size = new System.Drawing.Size(90, 28);
+			this.button_addList.TabIndex = 0;
+			this.button_addList.Text = "添加文件列表";
+			this.button_addList.UseVisualStyleBackColor = true;
+			this.button_addList.Click += new System.EventHandler(this.addList_Click);
+			// 
+			// listBox_allFileName
+			// 
+			this.listBox_allFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.listBox_allFileName.FormattingEnabled = true;
+			this.listBox_allFileName.ItemHeight = 12;
+			this.listBox_allFileName.Location = new System.Drawing.Point(12, 12);
+			this.listBox_allFileName.Name = "listBox_allFileName";
+			this.listBox_allFileName.Size = new System.Drawing.Size(184, 424);
+			this.listBox_allFileName.TabIndex = 26;
+			this.listBox_allFileName.SelectedIndexChanged += new System.EventHandler(this.listBox_allFileName_SelectedIndexChanged);
+			// 
+			// button_clearList
+			// 
+			this.button_clearList.Location = new System.Drawing.Point(202, 46);
+			this.button_clearList.Name = "button_clearList";
+			this.button_clearList.Size = new System.Drawing.Size(90, 28);
+			this.button_clearList.TabIndex = 27;
+			this.button_clearList.Text = "清空文件列表";
+			this.button_clearList.UseVisualStyleBackColor = true;
+			this.button_clearList.Click += new System.EventHandler(this.clearList_Click);
+			// 
+			// pictureBox_imageOverlay
+			// 
+			this.pictureBox_imageOverlay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox_imageOverlay.Enabled = false;
+			this.pictureBox_imageOverlay.Location = new System.Drawing.Point(298, 12);
+			this.pictureBox_imageOverlay.Name = "pictureBox_imageOverlay";
+			this.pictureBox_imageOverlay.Size = new System.Drawing.Size(391, 424);
+			this.pictureBox_imageOverlay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox_imageOverlay.TabIndex = 1;
+			this.pictureBox_imageOverlay.TabStop = false;
+			// 
+			// button_saveImageOverlay
+			// 
+			this.button_saveImageOverlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button_saveImageOverlay.Location = new System.Drawing.Point(202, 406);
+			this.button_saveImageOverlay.Name = "button_saveImageOverlay";
+			this.button_saveImageOverlay.Size = new System.Drawing.Size(90, 28);
+			this.button_saveImageOverlay.TabIndex = 28;
+			this.button_saveImageOverlay.Text = "保存叠加图片";
+			this.button_saveImageOverlay.UseVisualStyleBackColor = true;
+			this.button_saveImageOverlay.Click += new System.EventHandler(this.saveImageOverlay_Click);
+			// 
+			// button_addImage
+			// 
+			this.button_addImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button_addImage.Location = new System.Drawing.Point(202, 289);
+			this.button_addImage.Name = "button_addImage";
+			this.button_addImage.Size = new System.Drawing.Size(90, 28);
+			this.button_addImage.TabIndex = 29;
+			this.button_addImage.Text = "添加 图片";
+			this.button_addImage.UseVisualStyleBackColor = true;
+			this.button_addImage.Click += new System.EventHandler(this.addImage_Click);
+			// 
+			// button_removeImage
+			// 
+			this.button_removeImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button_removeImage.Location = new System.Drawing.Point(202, 323);
+			this.button_removeImage.Name = "button_removeImage";
+			this.button_removeImage.Size = new System.Drawing.Size(90, 28);
+			this.button_removeImage.TabIndex = 30;
+			this.button_removeImage.Text = "删除 图片";
+			this.button_removeImage.UseVisualStyleBackColor = true;
+			this.button_removeImage.Click += new System.EventHandler(this.removeImage_Click);
+			// 
+			// pictureBox_preview
+			// 
+			this.pictureBox_preview.Enabled = false;
+			this.pictureBox_preview.Location = new System.Drawing.Point(202, 113);
+			this.pictureBox_preview.Margin = new System.Windows.Forms.Padding(0);
+			this.pictureBox_preview.Name = "pictureBox_preview";
+			this.pictureBox_preview.Size = new System.Drawing.Size(90, 90);
+			this.pictureBox_preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox_preview.TabIndex = 31;
+			this.pictureBox_preview.TabStop = false;
+			// 
+			// label_offsetX
+			// 
+			this.label_offsetX.AutoSize = true;
+			this.label_offsetX.Location = new System.Drawing.Point(202, 209);
+			this.label_offsetX.Name = "label_offsetX";
+			this.label_offsetX.Size = new System.Drawing.Size(47, 12);
+			this.label_offsetX.TabIndex = 32;
+			this.label_offsetX.Text = "OffsetX";
+			this.label_offsetX.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			// 
+			// textBox_inputOffsetX
+			// 
+			this.textBox_inputOffsetX.Location = new System.Drawing.Point(246, 206);
+			this.textBox_inputOffsetX.MaxLength = 6;
+			this.textBox_inputOffsetX.Name = "textBox_inputOffsetX";
+			this.textBox_inputOffsetX.Size = new System.Drawing.Size(46, 21);
+			this.textBox_inputOffsetX.TabIndex = 33;
+			this.textBox_inputOffsetX.Text = "0";
+			this.textBox_inputOffsetX.TextChanged += new System.EventHandler(this.textBox_inputOffsetX_TextChanged);
+			this.textBox_inputOffsetX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_inputOffset_XY_KeyPress);
+			// 
+			// label_OffsetY
+			// 
+			this.label_OffsetY.AutoSize = true;
+			this.label_OffsetY.Location = new System.Drawing.Point(202, 236);
+			this.label_OffsetY.Name = "label_OffsetY";
+			this.label_OffsetY.Size = new System.Drawing.Size(47, 12);
+			this.label_OffsetY.TabIndex = 34;
+			this.label_OffsetY.Text = "OffsetY";
+			// 
+			// textBox_inputOffsetY
+			// 
+			this.textBox_inputOffsetY.Location = new System.Drawing.Point(246, 233);
+			this.textBox_inputOffsetY.MaxLength = 5;
+			this.textBox_inputOffsetY.Name = "textBox_inputOffsetY";
+			this.textBox_inputOffsetY.Size = new System.Drawing.Size(46, 21);
+			this.textBox_inputOffsetY.TabIndex = 35;
+			this.textBox_inputOffsetY.Text = "0";
+			this.textBox_inputOffsetY.TextChanged += new System.EventHandler(this.textBox_inputOffsetY_TextChanged);
+			this.textBox_inputOffsetY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_inputOffset_XY_KeyPress);
+			// 
+			// button_clearImageOverlay
+			// 
+			this.button_clearImageOverlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button_clearImageOverlay.Location = new System.Drawing.Point(202, 372);
+			this.button_clearImageOverlay.Name = "button_clearImageOverlay";
+			this.button_clearImageOverlay.Size = new System.Drawing.Size(90, 28);
+			this.button_clearImageOverlay.TabIndex = 36;
+			this.button_clearImageOverlay.Text = "清除叠加图片";
+			this.button_clearImageOverlay.UseVisualStyleBackColor = true;
+			this.button_clearImageOverlay.Click += new System.EventHandler(this.button_clearImageOverlay_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(701, 446);
+			this.Controls.Add(this.button_clearImageOverlay);
+			this.Controls.Add(this.textBox_inputOffsetY);
+			this.Controls.Add(this.label_OffsetY);
+			this.Controls.Add(this.textBox_inputOffsetX);
+			this.Controls.Add(this.label_offsetX);
+			this.Controls.Add(this.pictureBox_preview);
+			this.Controls.Add(this.button_removeImage);
+			this.Controls.Add(this.button_addImage);
+			this.Controls.Add(this.button_saveImageOverlay);
+			this.Controls.Add(this.button_clearList);
+			this.Controls.Add(this.listBox_allFileName);
+			this.Controls.Add(this.button_addList);
+			this.Controls.Add(this.pictureBox_imageOverlay);
+			this.Name = "Form1";
+			this.Text = "图片叠加";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_imageOverlay)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_preview)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Button button_addList;
+		private Button button_clearList;
+		private PictureBox pictureBox_imageOverlay;
+		private ListBox listBox_allFileName;
+		private Button button_saveImageOverlay;
+		private Button button_addImage;
+		private Button button_removeImage;
+		private PictureBox pictureBox_preview;
+		private Label label_offsetX;
+		private Label label_OffsetY;
+		private TextBox textBox_inputOffsetX;
+		private TextBox textBox_inputOffsetY;
+		private Button button_clearImageOverlay;
+	}
+}
+
---
+++ OtherTools/ImageOverlay/ImageOverlay/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.CheckedListBox;
+using static System.Windows.Forms.ListBox;
+
+/// <summary>
+/// @Kimi 2018/12/17 15:24
+/// </summary>
+namespace ImageOverlay
+{
+	public partial class Form1 : Form
+	{
+		private string lastpath = @"E:\WorkSpace\client_Manghuangji_Trunk_Android\Images2\MainUI";
+		
+		//选择文件名称
+		List<string> selectedFileNames = new List<string>();
+
+		//记录XY的偏移值
+		Dictionary<string, KeyValuePair<int, int>> preview_offsetXY = new Dictionary<string, KeyValuePair<int, int>>();
+
+		//需要保存的图片内容
+		Bitmap saveBitmap = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//System.Console.WriteLine("Form1_Load");
+		}
+
+		private void addList_Click(object sender, EventArgs e)  //add listBox_allFileName
+		{
+			string filePath = null;
+			IEnumerable<string> files = null;
+			using (var fbd = new FolderBrowserDialog())
+			{
+				if (lastpath != "")
+				{
+					fbd.SelectedPath = lastpath;
+				}
+				DialogResult result = fbd.ShowDialog();
+				if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					files = Directory.EnumerateFiles(fbd.SelectedPath, "*.*", SearchOption.TopDirectoryOnly)
+						.Where(s => s.EndsWith(".png") || s.EndsWith(".jpg"));
+					files = files.Select(s => Path.GetFileName(s));
+					filePath = fbd.SelectedPath;
+
+					if (files.Count<string>() == 0)
+					{
+						System.Windows.Forms.MessageBox.Show("No images found in folder!");
+						return;
+					}
+				}
+				else
+				{
+					return;
+				}
+				lastpath = fbd.SelectedPath;
+			}
+
+			listBox_allFileName.Items.Clear();
+			listBox_allFileName.Items.AddRange(files.ToArray());
+			listBox_allFileName.Tag = filePath;
+
+			pictureBox_preview.Image = null;
+			selectedFileNames.Clear();
+			preview_offsetXY.Clear();
+		}
+
+		private void clearList_Click(object sender, EventArgs e)
+		{
+			listBox_allFileName.Items.Clear();
+			pictureBox_preview.Image = null;
+			selectedFileNames.Clear();
+			preview_offsetXY.Clear();
+		}
+
+		private void listBox_allFileName_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (sender is ListBox)
+			{
+				ListBox listBox = sender as ListBox;
+				Bitmap result = new Bitmap(90, 90);
+				Graphics g = Graphics.FromImage(result);
+				g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+				g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+				g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+				var path = Path.Combine(listBox.Tag.ToString(), listBox.SelectedItem.ToString());
+				using (Image temp_img = Bitmap.FromFile(path))
+				{
+					g.DrawImage(temp_img, new Rectangle(0, 0, 90, 90));
+				}
+
+				// preview
+				pictureBox_preview.Tag = listBox.SelectedItem.ToString();
+				pictureBox_preview.Image = result;
+			}
+		}
+
+
+		private void button_clearImageOverlay_Click(object sender, EventArgs e)
+		{
+			pictureBox_imageOverlay.Image = null;
+			selectedFileNames.Clear();
+			preview_offsetXY.Clear();
+		}
+
+		private void saveImageOverlay_Click(object sender, EventArgs e)
+		{
+			if (saveBitmap == null)
+			{
+				System.Windows.Forms.MessageBox.Show("No image to save!");
+				return;
+			}
+			using (var sfd = new SaveFileDialog())
+			{
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					//if (checkBox3.Checked == true)
+					//	using (var crp = CropBitmap(tosave))
+					//	{
+					//		crp.Save(sfd.FileName, ImageFormat.Png);
+					//	}
+					//else
+					{
+						saveBitmap.Save(sfd.FileName, ImageFormat.Png);
+					}
+				}
+			}
+		}
+
+		private void removeImage_Click(object sender, EventArgs e)
+		{
+			var fileName = "";
+			foreach (var item in listBox_allFileName.SelectedItems)
+			{
+				fileName = item.ToString();
+				if (string.IsNullOrEmpty(fileName) || !selectedFileNames.Contains(fileName))
+				{
+					continue;
+				}
+				selectedFileNames.Remove(fileName);
+			}
+
+			update_image();
+		}
+
+		private void addImage_Click(object sender, EventArgs e)
+		{
+			var fileName = "";
+			foreach (var item in listBox_allFileName.SelectedItems)
+			{
+				fileName = item.ToString();
+				if (string.IsNullOrEmpty(fileName) || selectedFileNames.Contains(fileName))
+				{
+					continue;
+				}
+				selectedFileNames.Add(fileName);
+			}
+
+			update_image();
+		}
+
+		private void update_image()
+		{
+			// reset
+			pictureBox_imageOverlay.Image = null;
+
+			// calc max value
+			int imgMax_W = 0;
+			int imgMax_H = 0;
+			foreach (var item in selectedFileNames)
+			{
+				var path = Path.Combine(listBox_allFileName.Tag.ToString(), item.ToString());
+				System.Console.WriteLine("path:" + path);
+				using (Image temp_img = Bitmap.FromFile(path))
+				{
+					if (imgMax_W < temp_img.Width)
+						imgMax_W = temp_img.Width;
+					if (imgMax_H < temp_img.Height)
+						imgMax_H = temp_img.Height;
+				}
+			}
+
+			if (imgMax_W == 0 || imgMax_H == 0)
+			{
+				return;
+			}
+
+			// DrawImage
+			Bitmap result = new Bitmap(imgMax_W, imgMax_H);
+			Graphics g = Graphics.FromImage(result);
+
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+			g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+			foreach (string fileName in selectedFileNames)
+			{
+				if (string.IsNullOrEmpty(fileName))
+				{
+					continue;
+				}
+				var path = Path.Combine(listBox_allFileName.Tag.ToString(), fileName.ToString());
+				using (Image temp_img = Bitmap.FromFile(path))
+				{
+					int sPos = (int)((imgMax_W - temp_img.Width) * 0.5f);
+					int ePos = (int)((imgMax_H - temp_img.Height) * 0.5f);
+
+					if (preview_offsetXY.ContainsKey(fileName))
+					{
+						sPos += preview_offsetXY[fileName].Key;
+						ePos += preview_offsetXY[fileName].Value;
+					}
+
+					g.DrawImage(temp_img, new Rectangle(sPos, ePos, temp_img.Width, temp_img.Height));
+				}
+			}
+
+			pictureBox_imageOverlay.Image = result;
+			saveBitmap = result;
+		}
+
+		private void textBox_inputOffsetX_TextChanged(object sender, EventArgs e)
+		{
+			TextBox textBox = sender as TextBox;
+			string key = pictureBox_preview.Tag.ToString();
+			int offsetX = 0;
+
+			if (int.TryParse(textBox.Text, out offsetX))
+			{
+				if (preview_offsetXY.ContainsKey(key))
+				{
+					preview_offsetXY[key] = new KeyValuePair<int, int>(offsetX, preview_offsetXY[key].Value);
+				}
+				else
+				{
+					preview_offsetXY[key] = new KeyValuePair<int, int>(offsetX, 0);
+				}
+			}
+			
+			update_image();
+		}
+
+		private void textBox_inputOffsetY_TextChanged(object sender, EventArgs e)
+		{
+			TextBox textBox = sender as TextBox;
+			string key = pictureBox_preview.Tag.ToString();
+			int offsetY = 0;
+
+			if (int.TryParse(textBox.Text, out offsetY))
+			{
+				if (preview_offsetXY.ContainsKey(key))
+				{
+					preview_offsetXY[key] = new KeyValuePair<int, int>(preview_offsetXY[key].Value, offsetY);
+				}
+				else
+				{
+					preview_offsetXY[key] = new KeyValuePair<int, int>(0, offsetY);
+				}
+			}
+
+			update_image();
+		}
+
+		private void textBox_inputOffset_XY_KeyPress(object sender, KeyPressEventArgs e)//控制键盘输入内容
+		{
+			//System.Console.WriteLine(e.KeyChar);
+
+			if (e.KeyChar == (char)45)
+			{
+				return;//负号处理，可以输入
+			}
+
+			if (!Char.IsNumber(e.KeyChar) && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+			{
+				e.Handled = true;
+				//System.Console.WriteLine("请输入数字：" + e.KeyChar.ToString());
+			}
+		}
+	}
+}
